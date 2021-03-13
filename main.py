f68e0451cbd5a33713740b3dab87924f5c5e6adc
timothyyambao <sosicck@gmail.com>
2021-03-13 23:19:09 +0000

    added server app keep_alive to check if server is up.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from replit import db
+from keep_alive import keep_alive
 
 client = discord.Client()
 
@@ -58,4 +59,5 @@ async def on_message(message):
       await message.channel.send(db[message.guild.id])
       del db[message.guild.id]
 
+keep_alive()
 client.run(os.getenv('TOKEN')) 
